2806555f11f95ec416b8d357b9c8bdd10d9cf5a2
Shahzad Siddique <malikshahzad454545@gmail.com>
2025-01-29 23:31:47 +0500

    header changes

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,11 +1,11 @@
-import { Search } from 'lucide-react'
+import { Home, Search } from 'lucide-react'
 import { ReactNode } from 'react';
 import { Link, useLocation } from 'react-router-dom'
 import TrendingSection from './components/TrendingSection';
 
 interface LayoutProps {
     children: ReactNode;
-    hideSidebar?:boolean;
+    hideSidebar?: boolean;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children, hideSidebar }) => {
@@ -15,7 +15,7 @@ const Layout: React.FC<LayoutProps> = ({ children, hideSidebar }) => {
             {/* header */}
             <header className="fixed top-0 w-full bg-gray-900 z-50 p-4 py-[16px] border-b border-gray-800">
                 <div className="container flex items-center justify-between">
-                    <div className="flex items-center gap-4">
+                    <div className="flex w-full items-center gap-4">
                         <div className="relative">
                             <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5" />
                             <input
@@ -24,6 +24,9 @@ const Layout: React.FC<LayoutProps> = ({ children, hideSidebar }) => {
                                 className="pl-12 pr-7 py-4 max-w-[181px] w-full max-h-10 bg-gray-800 rounded-full text-md focus:outline-none focus:border-transparent"
                             />
                         </div>
+                        {/* <div className='flex justify-center items-center w-full'>
+                            <Home />
+                        </div> */}
                     </div>
                 </div>
             </header>

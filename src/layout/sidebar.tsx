e1582094948d407b1c4afb308cbf4ff899609ee0
Shahzad Siddique <malikshahzad454545@gmail.com>
2025-01-31 11:40:41 +0500

    container fixes

--- src/layout/sidebar.tsx
+++ src/layout/sidebar.tsx
@@ -51,7 +51,7 @@ const Sidebar = ({isRightSide}: Props) => {
 
   return (
     <div className="pb-5">
-      <h2 className={`text-2xl pl-6 md:text-2xl font-[500px] text-center ${!isRightSide ? "text-white" : "text-black"} fixed top-3 z-10 bg-black w-[300px] py-5 rounded-[20px]`}>Categories</h2>
+      <h2 className={`text-2xl pl-6 md:text-2xl font-[500px] text-center text-white fixed top-3 z-10 bg-black w-[300px] py-5 rounded-[20px] min-h-[72px]`}>{!isRightSide && "Categories"}</h2>
       <div className="w-full p-6 pt-20 h-full">
         {isLoading ? <CardsSkeleton /> : trending.map((item, index) => {
           if (item.type === 'channel') {

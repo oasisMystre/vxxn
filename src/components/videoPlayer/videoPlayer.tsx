0b45cc43645036761ee3b0638b2d3f9ca47eecb0
Shahzad Siddique <malikshahzad454545@gmail.com>
2025-02-01 14:27:55 +0500

    search page fixes

--- src/components/videoPlayer/videoPlayer.tsx
+++ src/components/videoPlayer/videoPlayer.tsx
@@ -62,7 +62,7 @@ function VideoPlayerModal({isRightSide}: Props) {
         <div className={`w-full h-full ${!isRightSide && "p-5"}`}>
             {/* Video Player Section */}
             <div className="rounded-[20px] flex justify-center items-center h-full">
-                <div className={`relative ${isRightSide ? "w-full h-[216px]" : "max-h-[450px] h-full w-full max-w-[500px]"} rounded-[20px]`}>
+                <div className={`relative ${isRightSide ? "w-full h-[400px]" : "max-h-[450px] h-full w-full max-w-[450px]"} rounded-[20px]`}>
                     <video
                         className="absolute top-0 left-0 w-full h-full rounded-[20px] object-cover"
                         poster="https://images.unsplash.com/photo-1516035069371-29a1b244cc32?w=1280&h=720&fit=crop"
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../../layout/layout';
 import Sidebar from '../../layout/sidebar';
 import VideoPlayerModal from '../../components/videoPlayer/videoPlayer';
-import { Link, useLocation } from 'react-router-dom';
-import { CameraIcon, HomeIcon, SearchIcon } from 'lucide-react';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { CameraIcon, ChevronRight, HomeIcon, SearchIcon } from 'lucide-react';
+import { VideoModal } from '../../components/videoModal/videoModal';
 
 function Home() {
     const [posts, setPosts] = useState<number[]>([]);
@@ -40,11 +41,26 @@ function Home() {
     );
 
     const [active, setActive] = React.useState("home");
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const searchValue = searchParams.get("search") || "";
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e?.target);
+        const searchValue = formData.get("search") || "";
+        searchParams.set("search", searchValue as string);
+        setSearchParams(searchParams)
+    }
+    React.useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <Layout>
             <div className="fixed lg:block hidden border-none left-3 top-3">
-                <div style={{ width: "300px", height: "calc(100vh - 24px)" }} className="artboard phone-1 bg-black rounded-[20px] overflow-y-auto no-scrollbar">
-                    <Sidebar />
+                <div style={{ width: "300px", height: "calc(100vh - 24px)" }} className="artboard phone-1 bg-black rounded-[20px] flex justify-center items-center">
+                    <div style={{ width: "270px", height: "calc(100vh - 50px)" }} className="artboard phone-1 bg-[#121212] rounded-[20px]">
+                    </div>
                 </div>
             </div>
             <div className="flex-1 min-h-screen">
@@ -58,20 +74,139 @@ function Home() {
                             <Link to="#" onClick={() => setActive("home")}>
                                 <HomeIcon className={`w-6 h-6 cursor-pointer ${active == "home" && "text-white"} text-gray-500`} />
                             </Link>
-                            <Link to="/upload" onClick={() => setActive("camera")}>
-                                <CameraIcon className={`w-6 h-6 cursor-pointer ${active == "camera" && "text-white"} text-gray-500`} />
-                            </Link>
+                            <div className='md:block hidden'>
+                                <Link to="/upload" onClick={() => setActive("camera")}>
+                                    <CameraIcon className={`w-6 h-6 cursor-pointer ${active == "camera" && "text-white"} text-gray-500`} />
+                                </Link>
+                            </div>
                         </div>
+                        {active == "search" && <div className={`pt-10 ${searchValue ? "pb-[20px]" : "pb-[60px]"} fixed top-[60px] bg-black z-10 lg:max-w-[calc(100vw-650px)] max-w-[calc(100vw-24px)] w-full md:px-0 px-10`}>
+                            <form onSubmit={handleSearch} className="flex justify-center items-center">
+                                <input defaultValue={searchValue} name='search' type="text" placeholder="What do you want to watch ?" className="input input-bordered bg-[#1e1e1e] h-[40px] text-white max-w-[550px] w-full" />
+                            </form>
+                            {searchValue && <p className='mt-5 text-center font-semi text-[20px]'>Search Results for "{searchValue}"</p>}
+                        </div>}
 
                         {isLoading
                             ? Array.from({ length: 3 }).map((_, i) => (
                                 <PostSkeleton key={i} />
                             ))
-                            : posts.map((post) => (
+                            : active == "home" ? posts.map((post) => (
                                 <div key={post} className="mb-6 mt-10 h-full overflow-y-auto">
                                     <VideoPlayerModal />
                                 </div>
-                            ))}
+                            )) :
+                                <div className={`w-full justify-center px-3 ${searchValue ? "mt-[220px]" : "mt-[180px]" }`}>
+                                    <div className='flex justify-between'>
+                                        <p className='text-white text-md font-bold mb-10 md:pl-7 pl-3'>Trending This Week</p>
+                                        <p className='text-white text-md font-bold mb-10 md:pl-7 pr-3 flex items-center '>See all <ChevronRight /></p>
+                                    </div>
+                                    <div className="carousel carousel-center lg:max-w-[calc(100vw-680px)] max-w-[calc(100vw-44px)] gap-10">
+                                        <div className="carousel-item max-h-[130px] max-w-[150px]">
+                                            <img className='rounded-[20px]' src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" alt="Pizza" />
+                                        </div>
+                                        <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                            <img
+                                                className='rounded-[20px]'
+                                                src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+                                                alt="Pizza" />
+                                        </div>
+                                        <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                            <img
+                                                className='rounded-[20px]'
+                                                src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+                                                alt="Pizza" />
+                                        </div>
+                                        <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                            <img
+                                                className='rounded-[20px]'
+                                                src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+                                                alt="Pizza" />
+                                        </div>
+                                        <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                            <img className='rounded-[20px]' src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" alt="Pizza" />
+                                        </div>
+                                        <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                            <img className='rounded-[20px]' src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" alt="Pizza" />
+                                        </div>
+                                        <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                            <img
+                                                className='rounded-[20px]'
+                                                src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+                                                alt="Pizza" />
+                                        </div>
+                                    </div>
+                                    <div className='w-full my-10'>
+                                        <div className='flex justify-between'>
+                                            <p className='text-white text-md font-bold mb-10 md:pl-7 pl-3'>Categories</p>
+                                            <p className='text-white text-md font-bold mb-10 md:pl-7 pr-3 flex items-center '>See all <ChevronRight /></p>
+                                        </div>
+                                        <div className='flex justify-center '>
+                                            <div className="bg-[#121212] p-5 rounded-[20px] h-full flex gap-10 justify-center items-between lg:max-w-[calc(100vw-700px)] max-w-[calc(100vw-34px)] w-full">
+                                                <div className='flex flex-col gap-3 justify-center items-center'>
+                                                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
+                                                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
+                                                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
+                                                </div>
+                                                <div className='flex flex-col gap-3 justify-center items-center'>
+                                                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
+                                                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
+                                                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
+                                                </div>
+                                                <div className='flex flex-col gap-3 justify-center items-center'>
+                                                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
+                                                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
+                                                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className='w-full my-10'>
+                                        <div className='flex justify-between'>
+                                            <p className='text-white text-md font-bold mb-10 md:pl-7 pl-3'>New Creators</p>
+                                            <p className='text-white text-md font-bold mb-10 md:pl-7 pr-3 flex items-center '>See all <ChevronRight /></p>
+                                        </div>
+                                        <div>
+                                            <div className="carousel carousel-center lg:max-w-[calc(100vw-680px)] max-w-[calc(100vw-44px)] gap-10">
+                                                <div className="carousel-item max-h-[130px] max-w-[150px]">
+                                                    <img className='rounded-[20px]' src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" alt="Pizza" />
+                                                </div>
+                                                <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                                    <img
+                                                        className='rounded-[20px]'
+                                                        src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+                                                        alt="Pizza" />
+                                                </div>
+                                                <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                                    <img
+                                                        className='rounded-[20px]'
+                                                        src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+                                                        alt="Pizza" />
+                                                </div>
+                                                <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                                    <img
+                                                        className='rounded-[20px]'
+                                                        src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+                                                        alt="Pizza" />
+                                                </div>
+                                                <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                                    <img className='rounded-[20px]' src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" alt="Pizza" />
+                                                </div>
+                                                <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                                    <img className='rounded-[20px]' src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" alt="Pizza" />
+                                                </div>
+                                                <div onClick={() => setIsModalOpen(true)} className="cursor-pointer carousel-item max-h-[130px] max-w-[150px]">
+                                                    <img
+                                                        className='rounded-[20px]'
+                                                        src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+                                                        alt="Pizza" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                        }
                     </div>
                 </div>
             </div>
@@ -83,6 +218,7 @@ function Home() {
                     <Sidebar isRightSide />
                 </div>
             </div>
+            {isModalOpen && <VideoModal imageUrl="" onClose={() => setIsModalOpen(false)} />}
         </Layout>
     );
 }

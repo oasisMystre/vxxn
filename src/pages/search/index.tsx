aa1b19d9d0f208c65e9e140f3334a33a45a22e20
Include caleb.fun <genos@saitama.fun>
2025-02-09 21:02:30 +0100

    feat: fix slider browser compability

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -2,7 +2,7 @@ import SearchInput from "../../components/SearchInput";
 
 export default function SearchPage() {
   return (
-    <div className="absolute inset-0 p-2 bg-black rounded-xl">
+    <div className="absolute inset-0 p-2 bg-black rounded-xl md:my-4">
       <div className="flex flex-col pt-16">
         <SearchInput
           className="self-center w-sm"
